047bdc31991478ae38cfe9b15776bfc1c9fbab68
jisham-vallikkadan <jishamvallikkadan@gmail.com>
2023-01-06 16:05:24 +0530

    commit

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -24,133 +24,135 @@ class Loginpage extends StatelessWidget {
     var _Povider = Provider.of<FluttertaskProvider>(context, listen: false);
 
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            children: [
-              Container(
-                // decoration: BoxDecoration(color: Colors.red,
-                //     image: DecorationImage(
-                //         image: AssetImage(
-                //             "images/img.png"
-                //         ),
-                //         fit: BoxFit.cover
-                //     )
-                // ),
-                color: Colors.red,
-                width: double.infinity,
-                height: 250,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Container(
+                  // decoration: BoxDecoration(color: Colors.red,
+                  //     image: DecorationImage(
+                  //         image: AssetImage(
+                  //             "images/img.png"
+                  //         ),
+                  //         fit: BoxFit.cover
+                  //     )
+                  // ),
+                  color: Colors.red,
+                  width: double.infinity,
+                  height: 250,
 
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 30, bottom: 30),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Welcome',
-                        style: TextStyle(
-                            fontSize: 30,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        'Manage your Bus and Drivers',
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w500),
-                      ),
-                    ],
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 30, bottom: 30),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Welcome',
+                          style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          'Manage your Bus and Drivers',
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              // SizedBox(
-              //   height: 20,
-              // ),
-              Container(
-                margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-                child: TextField(
-                  controller: usernamecontroller,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                      fillColor: Color(0xffeaeaea),
-                      filled: true,
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide.none),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide.none),
-                      hintText: 'Enter Username',
-                      hintStyle: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          color: Colors.grey[600])),
+                // SizedBox(
+                //   height: 20,
+                // ),
+                Container(
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  child: TextField(
+                    controller: usernamecontroller,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                        fillColor: Color(0xffeaeaea),
+                        filled: true,
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none),
+                        hintText: 'Enter Username',
+                        hintStyle: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey[600])),
+                  ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-                child: TextField(
-                  controller: passwordcontroler,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                      fillColor: Color(0xffeaeaea),
-                      filled: true,
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide.none),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide.none),
-                      hintText: 'Enter Password',
-                      hintStyle: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          color: Colors.grey[600])),
+                Container(
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  child: TextField(
+                    controller: passwordcontroler,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                        fillColor: Color(0xffeaeaea),
+                        filled: true,
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none),
+                        hintText: 'Enter Password',
+                        hintStyle: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey[600])),
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 20),
-            child: Button(
-              click: () async {
-                SharedPreferences prefer =
-                    await SharedPreferences.getInstance();
-                status = prefer.getBool('status');
-                token = prefer.getString('token');
-                print(token);
-                _Povider.login(
-                  {
-                    "username": usernamecontroller.text,
-                    "password": passwordcontroler.text
-                  },
-                );
-
-                if (status == true) {
-                  Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Buslist(),
-                      ));
-                } else {
-                  Fluttertoast.showToast(msg: 'Enter values');
-                }
-              },
-              buttontext: 'Login',
-              buttonheight: 50,
-              buttondecoration: BoxDecoration(
-                  color: Colors.red, borderRadius: BorderRadius.circular(10)),
-              buttontextfontsize: 17,
-              buttonmarginright: 20,
-              buttonmarginleft: 20,
-              buttontextcolour: Colors.white,
+              ],
             ),
-          ),
-        ],
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: Button(
+                click: () async {
+                  SharedPreferences prefer =
+                      await SharedPreferences.getInstance();
+                  status = prefer.getBool('status');
+                  token = prefer.getString('token');
+                  print(token);
+                  _Povider.login(
+                    {
+                      "username": usernamecontroller.text,
+                      "password": passwordcontroler.text
+                    },
+                  );
+
+                  if (status == true) {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Buslist(),
+                        ));
+                  } else {
+                    Fluttertoast.showToast(msg: 'Enter values');
+                  }
+                },
+                buttontext: 'Login',
+                buttonheight: 50,
+                buttondecoration: BoxDecoration(
+                    color: Colors.red, borderRadius: BorderRadius.circular(10)),
+                buttontextfontsize: 17,
+                buttonmarginright: 20,
+                buttonmarginleft: 20,
+                buttontextcolour: Colors.white,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
